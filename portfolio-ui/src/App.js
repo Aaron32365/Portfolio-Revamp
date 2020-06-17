b723ca65fd4b1b135377dd659b4fcbac077691c9
Aaron32365 <56372224+Aaron32365@users.noreply.github.com>
2020-06-17 14:39:06 -0700

    navbar added with routing functionality and css for navbar added

--- portfolio-ui/src/App.js
+++ portfolio-ui/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import SideNav from "./components/sideNav/sideNav"
+import SideNav from "./components/sideNav/sideNav.js"
 import { BrowserRouter as Router, Route} from "react-router-dom"; //edited file
 import Home from "./pages/Home"
 
@@ -9,8 +9,8 @@ function App() {
   return (
     <div className="App">
       <Router>
-        {/* <SideNav/> */}
-        <Route exact path="/" component={Home}/>
+        <SideNav/>
+        <Route exact path="/" component={Home}/>{/** add in  auto reroute to /Portfolio by default -- portfolio is main page*/}
       </Router>
     </div>
   );
--- portfolio-ui/src/components/sideNav/sideNav.js
+++ portfolio-ui/src/components/sideNav/sideNav.js
@@ -1,21 +1,56 @@
 import React, {useState} from "react"
-import { NavLink, useHistory } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+import "./sideNav.css"
 
 export default function SideNav(){
 
-    const [navState, setNavState] = useState("0")
+    const [navState, setNavState] = useState({
+        width: "250px"
+    })
 
     function closeNav(){
-      setNavState("250px")
-    }
+        setTimeout(() => {
+            setNavState({...navState, width: "0px"})
+            console.log("Closing Sidenav...")
+        }, 600);
+      }
 
+      function openNav(){
+        setNavState({...navState, width: "250px"})
+        console.log("Opening Sidenav...")
+      }
+  
     return (
         <div>
-            <div id="mySidenav" class="sidenav">
-                <NavLink to="#" class="closebtn" onclick={closeNav()}></NavLink>
-                <NavLink to="/Portfolio" activeClassName="selected">Portfolio</NavLink>
-                <NavLink to="/About" activeClassName="selected">Services</NavLink>
-                <NavLink to="/Contact" activeClassName="selected">Contact</NavLink>
+            <div className="navbtn">
+                <span style={{cursor:"pointer", fontSize: "58px"}} onClick={openNav}>&#9776;</span>
+            </div>
+            <div className="sidenavs one-edge-shadow" id="mySidenav" style={{width: navState.width}}>
+
+                <NavLink to="" className="closebtn" onClick={closeNav}>x</NavLink>
+                <div className="row justify-content-center">
+                    {/* <div className="col-fluid"> */}
+                        <img src={require('./images/self.jpg')} alt="self"/>
+                    {/* </div> */}
+                </div>
+                <div className="row justify-content-center">
+                    <div className="col-fluid">
+                        <NavLink className="nav-item" to="/Portfolio" onClick={closeNav} activeClassName="selected">Portfolio</NavLink>
+                    </div>
+                </div>
+
+                <div className="row justify-content-center">
+                    <div className="col-fluid">
+                        <NavLink className="nav-item" to="/About" onClick={closeNav} activeClassName="selected">About</NavLink>
+                    </div>
+                </div>
+
+                <div className="row justify-content-center">
+                    <div className="col-fluid">
+                        <NavLink className="nav-item" to="/Contact" onClick={closeNav} activeClassName="selected">Contact</NavLink>
+                    </div>
+                </div>
+
             </div>
         </div>
     )
